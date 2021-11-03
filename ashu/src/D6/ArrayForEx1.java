527992fcea4026438a70114c95e73cb1692976c1
ashulikesneeze <93233523+ashulikesneeze@users.noreply.github.com>
2021-11-03 17:26:29 +0900

    ArrayFor

---
+++ ashu/src/D6/ArrayForEx1.java
@@ -0,0 +1,21 @@
+package D6;
+
+public class ArrayForEx1 {
+
+	public static void main(String[] args) {
+		int arr2[] = new int[5];
+		int i, sum;
+		for(i=0; i<=4; i++) {
+			arr2[i] = (i*2+2);
+		}
+		sum = 0;
+		//temp는 아래 반복문에서만 사용 가능
+		for( int tmp : arr2)
+			
+	
+		}
+			System.out.println("2부터 10까지의 합 : " +sum);
+
+	}
+
+}
