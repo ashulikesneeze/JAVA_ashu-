84328caab1ac6f3b1844d9c09ad74f65809a88f3
java_si <ksc970122@naver.com>
2021-11-04 17:25:52 +0900

    ArrayLength

--- ashu/src/D6/ArrayLengthEx1.java
+++ ashu/src/D6/ArrayLengthEx1.java
@@ -3,10 +3,12 @@ package D6;
 public class ArrayLengthEx1 {
 
 	public static void main(String[] args) {
-	int [] arr = new int [5];
-	System.out.println("배열의 길이 : " + arr.length);
-	int 	
-
+		int [] arr = new int [5];
+		System.out.println("배열의 길이 : " + arr.length);
+		int i;
+		for(i = 0; i<arr.length; i++) {
+			System.out.println("arr["+i+"] = " + arr[i]);
+		}
 	}
 
 }
