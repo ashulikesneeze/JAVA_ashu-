2b52dcab30473103ff9d5e1ac953b378d535666c
java_si <ksc970122@naver.com>
2021-11-04 17:26:08 +0900

    ArrayMultiD

--- ashu/src/D6/ArrayMultiDimentionEx1.java
+++ ashu/src/D6/ArrayMultiDimentionEx1.java
@@ -6,13 +6,13 @@ public class ArrayMultiDimentionEx1 {
 		/*구구단 2단에서 9단 전체를 배열에 저장하여 출력.
 		 * 0번지를 사용 안함. 편하게 보기위해.
 		 */
-	int [][]multipleTable = new int[10][10];
-	for(int i = 2; i<=9; i++) {
+		
+	int [][] multipleTable = new int[10][10];
+	for(int i = 2; i<= 9; i++) {
 		for(int j = 1; j<=9; j++) {
 			multipleTable[i][j] = i*j;
-			System.out.println(i + " X " +j + " = " + multipleTable);
+			System.out.println(i + " X " + j + " = " + multipleTable[i][j]);
 		}
 	}
-	
 	}
 }
