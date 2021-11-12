703a6f64c52be1d19f498d364693f0efce248df3
java_si <ksc970122@naver.com>
2021-11-12 09:28:09 +0900

    3

---
+++ ashu/src/D11/Square.java
@@ -0,0 +1,43 @@
+package D11;
+
+public class Square extends Rect {
+	
+	int side;
+	
+	public Square() {
+		//lt는 부모의 멤버변수이지만 private으로 되어있어서
+		//물려받기는 했지만 직접 접근 할 수 없다
+		//lt = newPoint(0,0);
+		//Point tmp = new Point(10.10);
+		//public으로 된 setter를 이용하여 lt에 접근해야 한다
+		//setlt(tmp);
+	}
+	public Square(Point pt, int side) {
+		//pt를 lt에 저장 
+		// lt=pt; lt에 접근권한이 없어서 에러 
+		//setLt(pt); lt와 밖에서 알려준 pt를 공유 
+		Point tmp = new Point(pt);
+		setLt(tmp);
+		
+		//side를 멤버변수 side에 저장
+		this.side = side; 
+	}
+	//resize는 메소드 오버로딩
+	public void resize(int side) {
+		this.side = side;
+	}
+	@Override
+	//Rect에서 물려받은 print() 메소드를 재정의 
+	public void print() {
+		System.out.println("---정사각형---");
+		System.out.println("시작점 : ");
+		//lt의 print 기능 호출
+		//lt.print(); 값을 수정하는 것이 아니기 때문에 
+		getLt().print();
+		
+		System.out.println("한 변의 길이 : " + side);
+		
+		
+	}
+
+}
