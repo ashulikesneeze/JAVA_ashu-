2df3b15448b255b228e1e6d9238e35d930d786d4
java_si <ksc970122@naver.com>
2021-11-12 17:17:08 +0900

    a

---
+++ ashu/src/D13/ExaEllipse.java
@@ -0,0 +1,9 @@
+package D13;
+//자동으로 상속 class 만들때!, override 
+public class ExaEllipse extends ExaShape {
+	
+	@Override
+	public void print() {
+		System.out.println("타원");
+	}
+}
---
+++ ashu/src/D13/ExaRect.java
@@ -0,0 +1,9 @@
+package D13;
+
+public class ExaRect extends ExaShape {
+
+	@Override
+	public void print() {
+		System.out.println("사각형");
+	}
+}
