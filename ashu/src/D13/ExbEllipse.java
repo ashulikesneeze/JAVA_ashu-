6ab268f1784d585885c5cb21dfad1c2c6919be84
java_si <ksc970122@naver.com>
2021-11-12 17:17:36 +0900

    b

---
+++ ashu/src/D13/ExbEllipse.java
@@ -0,0 +1,7 @@
+package D13;
+
+public class ExbEllipse extends ExbShape {
+	public void print() {
+		System.out.println("타원");
+	}
+}
---
+++ ashu/src/D13/ExbRect.java
@@ -0,0 +1,8 @@
+package D13;
+
+public class ExbRect extends ExbShape {
+	
+	public void print() {
+		System.out.println("사각형");
+	}
+}
