637c53cb4eceb660ca8f7b039b920fb18d36e189
java_si <ksc970122@naver.com>
2021-12-01 17:49:48 +0900

    D4

--- ashu/MYSQL/D4/D4-shoppingMall.sql
+++ ashu/MYSQL/D4/D4-shoppingMall.sql
@@ -140,3 +140,57 @@ REFERENCES `product` (
 	`pro_num`
 );
 
+insert into category(cat_main,cat_sub) values('아우터','점퍼'),('아우터','코트'),('상의','티셔츠'),
+('상의','블라우스/셔츠'),('하의','데님'),('하의','팬츠'),('하의','스커트');
+insert into product(pro_num,pro_name,pro_price,pro_detail,cat_num) values(1, '아크 폭스터 패딩',159000,'패딩입니다',1),
+(2, '앨비아 앙모코트',229000,'앙모코트입니다',2),(3, '클로 다운후드',99000,'다운 후드입니다',3),(4, '파코 레이어드탑',43000,'탑',4),
+(5, '기모 와이드 데님 팬츠',43000,'팬츠',5),(6, '롤스 버튼 팬츠',36000,'팬츠',6);
+
+ALTER TABLE `shoppingmall`.`product` 
+DROP COLUMN `pro_sub_category`,
+DROP COLUMN `pro_main_category`;
+
+-- 회원가입 순서1 : member에 insert => address에 insert => member에 update
+-- 회원가입 순서2 : address에 insert => member에 insert
+ 
+SELECT * FROM `option`;
+insert into address values(1,'청주시', '1234',null,'그린컴퓨터학원');
+insert into user values('abc123','abc123','홍길동','840613-1023697','010-1234-5678','남자',1);
+
+insert into `option` values(1, '화이트', 's', 10, 1);
+insert into `order` values(1, now(), 'abc123', 1, null, 1);
+update `option` set opt_stock = opt_stock -1 where opt_num = 1;
+insert into review values(1, '패딩좋아요','따뜻하고 좋아요',now(),'abc123', 1);
+
+
+ALTER TABLE `shoppingmall`.`product` 
+DROP FOREIGN KEY `FK_category_TO_product_1`;
+ALTER TABLE `shoppingmall`.`product` 
+CHANGE COLUMN `cat_num` `pro_cat_num` INT NOT NULL ;
+ALTER TABLE `shoppingmall`.`product` 
+ADD CONSTRAINT `FK_category_TO_product_1`
+  FOREIGN KEY (`pro_cat_num`)
+  REFERENCES `shoppingmall`.`category` (`cat_num`);
+
+select pro_name from product join category on cat_num = pro_cat_num where cat_main = '아우터';
+select pro_name from product join category on cat_num = pro_cat_num join `option` on opt_pro_num = pro_num
+where cat_main = '아우터' and opt_stock >= 1;    
+
+-- 가장 많이 팔린 탑 4 outer (환불,반품 제외) 
+select pro_name from product join category on cat_num = pro_cat_num join `option` on opt_pro_num = pro_num
+join `order` on opt_num = order_opt_num
+where cat_main ='아우터' group by pro_num order by sum(order_amount) desc limit 4;
+
+select count(*) as '아우터 제품의 갯수' from product join category on cat_num = pro_cat_num where cat_main ='아우터';
+-- 
+select * from product join category on cat_num = pro_cat_num where cat_main ='아우터' limit 10,10;
+
+select count(*) as 'abc123 회원이 주문한 횟수' from `order` where order_user_id = 'abc123';
+ 
+select sum(order_totalPrice) as 'abc123 회원의 상반기 사용 금액' from `order` where order_user_id = 'abc123' 
+and order_datetime like '2021%' and order_datetime < '2021-07-01'; 
+
+
+
+ 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
