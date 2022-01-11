6a7f1ab2d6885b3b4a991530cdcec337e451f020
ashulikesneeze <93233523+ashulikesneeze@users.noreply.github.com>
2022-01-11 09:27:51 +0900

    recap

--- ashu/src/Ashu_recap/studentInfoManagement.java
+++ ashu/src/Ashu_recap/studentInfoManagement.java
@@ -16,7 +16,50 @@ public class studentInfoManagement {
 		 */
 		
 		ArrayList<student> stdList = new ArrayList<student>();
+		Scanner scan = new Scanner(System.in);
+		int menu; 
+		
+		do { 
+			//메뉴 출력
+			printMenu();
+			//메뉴 선택
+			menu = scan.nextInt();
+			//선택된 메뉴에 따라 동작 실행 
+			switch(menu) {
+			//학생 정보 추가 기능 
+			case 1: inputStudent(scan, stdList);  break;
+			//전체 학생 정보 출력 기능 
+			case 2: printStudentList(stdList); break;
+			//특정 학생 정보 삭제 
+			case 3:
+				if(deleteStudent(stdList, scan)) {
+					System.out.println("학생 정보를 삭제했습니다.");
+				}else {
+					System.out.println("일치하는 학생 정보가 없습니다.");
+				}
+				break;
+			//특정 학생 정보 수정:
+			//이름, 성적만 수정할건지 (선택)
+			//학년, 반, 번호, 이름, 성적을 수정할건지 생각
+			case 4: 
+				if(modifyStudent(scan, stdList)) {
+					System.out.println("학생 정보를 수정했습니다.");
+				}else {
+					System.out.println("일치하는 학생 정보가 없습니다.");
+				}
+				break;
+			case 5: break;
+			default: 
+				System.out.println("잘못된 메뉴입니다.");
+				}
+			}while(menu != 5); 
+		
+		System.out.println("프로그램 종료합니다");
+		
+		scan.close();
+		}
+	
+	public static E
 
 	}
 
-}
